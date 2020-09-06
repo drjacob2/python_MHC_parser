65de42930b4d790e5ee90549286ed1f5804b230a
djracob2 <drjacob2@asu.edu>
2020-09-06 13:27:09 -0700

    update. This script update does not use dictionary of dictionary
    move $a1, $s1			# array1
    	move $a2, $s3			# length
    	move $a3, $t2			# howMany
    	li $a0, 1			# number = 1

--- mergefiles/readintodic0905_2.py
+++ mergefiles/readintodic0905_2.py
@@ -95,28 +95,16 @@ def gen_file_split(dict):
                 d_seq_gen.update({k_count:i})
                 count_exon+=1
             count+=1
-        for key, value in d_seq_gen.items():
-            print(key, "and", value, "\n")
+        #for key, value in d_seq_gen.items():
+            #print(key, "and", value, "\n")
     return d_seq_gen
 
 def count_introns(dict, length):
     l = []
     for v in dict.values():
-        #print(len(v))
         l.append(len(v))
     l = l[:(length+1)]
-    #print(l)
     return l
-
-def union_collections(d1, d2):
-    return { k: sorted(
-                    list(
-                        set(
-                            d1.get(k, []) + d2.get(k, [])
-                        )
-                    )
-                )
-             for k in set(d1.keys() + d2.keys()) }
 
 def main():
     nameList_nuc = []
@@ -127,75 +115,83 @@ def main():
     d_seq_nuc = {} 
     d_seq_gen= {}
     l = []
-
-    nuc_text = "DMA_nuc.txt"
+    nuc_text = "/Users/drjacobs/Documents/python_MHC_parser/mergefiles/DMA_nuc.txt"
     # nuc file parsing
     nameList_nuc, alleles_nuc = parsetodic(nuc_text)
     # print(nameList_nuc)
     # print(alleles_nuc)
 
     # gen file parsing 
-    gen_text = "DMA_gen.txt" 
+    gen_text = "/Users/drjacobs/Documents/python_MHC_parser/mergefiles/DMA_gen.txt" 
     nameList_gen, alleles_gen = parsetodic(gen_text)
-    print(nameList_gen)
-    print(alleles_gen)
+    #print(nameList_gen)
+    #print(alleles_gen)
+    for v in alleles_gen.values():
+        print(len(v))
+
+    for v in alleles_nuc.values():
+        print(len(v))
 
     # add more features to the dictionaries 
     # 1. count number of exon/intron switches in each file 
     value_bar_nuc = count_switch(alleles_nuc)
     value_bar_gen = count_switch(alleles_gen)
-    print(value_bar_nuc, 'and', value_bar_gen)
-    
-
-    # split the alleles in the gen file by the pipe (|) symbol
-    # sequence is a list of the new seperated intron/exon values
-    # add the sequence values to a dictionary defined 
-    # where key = allele and value is the list of intron/exon values 
-    d1 = {}
-    for k, v in alleles_gen.items():
-        print(k, 'and', v, "\n")
-        sequence = re.split('\|',v)
-        count_intron=0
-        count_exon=0
-        count=0
-        for i in sequence:
-            if count % 2 ==0:
-                k_count = k + ' intron-' + str(count_intron)
-                d1.update({k_count:i})
-                count_intron+=1
-            else:
-                k_count = k + ' exon-' + str(count_exon)
-                d1.update({k_count:i})
-                count_exon+=1
-            count+=1
-        alleles_gen[k].update(d1)
-        for key, value in alleles_gen.items():
-            print(key, "and", value, "\n")
-
 
     d_seq_nuc = nuc_file_split(alleles_nuc)
     d_seq_gen = gen_file_split(alleles_gen)
-    list_gen_values = count_introns(d_seq_gen, value_bar_gen)
+    list_gen_values = count_introns(d_seq_gen, value_bar_gen) # list of the lenght of gen entron and exon info. ex: [336, 88, 2145, 285, 629, 279, 211, 129, 402, 5, 504]
     print(list_gen_values)
+    list_nuc_values = count_introns(d_seq_nuc, value_bar_nuc)
+    print("Sum of Gen list_gen_values:", sum(list_gen_values))
+    print(list_nuc_values)
+    print("Sum of NUC list_nuc_values:",sum(list_nuc_values))
     list_intronsize = [] # list of the intron size information 
-    location_intron = []
+    location_intron = [] # list of intron location. This is based on where the exon is located. 
     for i in list_gen_values:
         if list_gen_values.index(i) % 2 == 0:
-            #print(i)
             list_intronsize.append(i)
         else: 
             location_intron.append(i)
     print(list_intronsize)
-    print(location_intron)
+    #print(location_intron)
+
+    # create list of intron inserts values 
+    a= [] # a will hold all of the intron values 
+    for i in list_intronsize:
+     #   (print(i))
+        a.append(i*'*')
+    for i in a:
+        print(len(i))
+    #print(a)
+    #first = ["HELLO WORLD!!!!", "FALLALALALLALALA", "WHAT's UPP", "Chocolate is Good", "I LOVE COOKIES", "PUPPIES ARE CUTE"]
+   # for j in range(len(first)):
+    for k, v in alleles_nuc.items():  # for key value in alleles.items()
+        j=0
+        newstr = ""
+        for i in range(len(v)):  # len(value)
+            c = v[i]  # get the base value at position i
+            if c == "|":  # check if it is |
+                # if it is - then add the first allele has in the same position to new string
+                    newstr += a[j]
+                    j+=1
+            else:
+                newstr += c  # else add value to new string
+        alleles_nuc[k] = newstr  # reset dictionary here
 
     for key in alleles_nuc.keys():
         if key in alleles_gen:
-            d[key] = alleles_gen[key]
+            print(key)
+            d.update(alleles_gen)
         else: 
+            print("NUC only:", key)
             d[key] = alleles_nuc[key]
-    
-   # print(d)
-
+    #print(d)
+    '''
+    for k, v in d.items():
+        print(k, v)
+    for v in d.values():
+        print(len(v))
+    '''
 
 if __name__ == "__main__":
     main()
